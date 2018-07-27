600f584d9a09e1186329e7832484bcfd798469cf
Jon Saw <jon.saw@gmail.com>
2018-07-27 12:58:14 +0800

    test custom error handler middleware https://github.com/sbstjn/appsync-resolvers/issues/8

--- middleware_test.go
+++ middleware_test.go
@@ -8,6 +8,24 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+type graphQLError struct {
+	Type    string      `json:"error_type"`
+	Message string      `json:"error_message"`
+	Data    interface{} `json:"error_data"`
+}
+
+func (e *graphQLError) Error() string {
+	return e.Message
+}
+
+func newGraphQLError(t string, m string, d interface{}) *graphQLError {
+	return &graphQLError{
+		Type:    t,
+		Message: m,
+		Data:    d,
+	}
+}
+
 func sequence(ch chan string, seq ...string) error {
 	for i, str := range seq {
 		if msg := <-ch; msg != str {
@@ -23,8 +41,6 @@ var _ = Describe("Middleware", func() {
 	}
 	type response struct {
 		Foo string
-	}
-	type graphQLError struct {
 	}
 
 	Context("With no hijacking", func() {
@@ -74,6 +90,61 @@ var _ = Describe("Middleware", func() {
 					"before 2",
 					"handler",
 					"after 2",
+					"after 1",
+				)).ToNot(HaveOccurred())
+		})
+	})
+
+	Context("With custom error middleware", func() {
+		ch := make(chan string, 10)
+		r := New()
+		r.Add("example.resolver", func(arg arguments) (*response, error) {
+			ch <- "handler"
+			return nil, newGraphQLError("BAD_REQUEST", "Invalid type", response{"bar"})
+		})
+		r.Use(func(h Handler) Handler {
+			m := func(in invocation) (interface{}, error) {
+				ch <- "before 1"
+				out, err := h.Serve(in)
+				ch <- "after 1"
+				return out, err
+			}
+			return HandlerFunc(m)
+		})
+		r.Use(func(h Handler) Handler {
+			m := func(in invocation) (interface{}, error) {
+				out, err := h.Serve(in)
+				if err != nil {
+					if errData, ok := err.(*graphQLError); ok {
+						return errData, nil
+					}
+				}
+				return out, err
+			}
+			return HandlerFunc(m)
+		})
+		res, err := r.Handle(invocation{
+			Resolve: "example.resolver",
+			Context: context{
+				Arguments: json.RawMessage(`{"bar":"foo"}`),
+			},
+		})
+
+		It("Should not error", func() {
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("Should have error data", func() {
+			Expect(res.(*graphQLError).Message).To(Equal("Invalid type"))
+			Expect(res.(*graphQLError).Type).To(Equal("BAD_REQUEST"))
+			Expect(res.(*graphQLError).Data.(response).Foo).To(Equal("bar"))
+		})
+
+		It("Should be in sequence", func() {
+			Expect(
+				sequence(ch,
+					"before 1",
+					"handler",
 					"after 1",
 				)).ToNot(HaveOccurred())
 		})
